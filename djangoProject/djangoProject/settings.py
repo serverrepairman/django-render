5bff188606aae0f2a079c429c6f6a2eb0f8d08bb
serverrepairman <gustjwkd1007@gmail.com>
2022-11-27 02:12:35 +0900

    update ALLOWED_HOSTS

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['seminar2022-week4.onrender.com']
